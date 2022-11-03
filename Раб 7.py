6dc739351f984103043e262ee33361bc0107fe91
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-11-03 16:45:28 +0300

    Update Раб 7.py

--- Раб 7.py
+++ Раб 7.py
@@ -1,3 +1,159 @@
+# 1.1.2(1,2.1)
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+
+class OurNeuralNetwork:
+    def __init__(self):
+        weights = np.array([0.5, 0.5, 0.5])
+        bias = 0
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.h3 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+
+    def feedforward(self, inputs):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+        out_h3 = self.h3.feedforward(x)
+        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2, out_h3]))
+        return out_o1
+
+
+network = OurNeuralNetwork()
+x = np.array([2, 3, 4])
+print(network.feedforward(x))
+
+# 1.1.2(1.2,2.2)
+import numpy as np
+
+
+def tanh(x):
+    return np.tan(x)
+
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+
+class OurNeuralNetwork:
+    def __init__(self):
+        weights = np.array([1, 0])
+        bias = 1
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+        self.o2 = Neuron(weights, bias)
+
+    def feedforward(self, inputs):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+        out_o2 = self.o2.feedforward(np.array([out_h1, out_h2]))
+        return out_o1, out_o2
+
+
+network = OurNeuralNetwork()
+x = np.array([2, 3])
+print(network.feedforward(x))
+
+# 1.1.2(2.3)
+import numpy as np
+
+
+def ReLU(x):
+    return np.max(0, x)
+
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+
+class OurNeuralNetwork:
+    def __init__(self):
+        weights = np.array([1, 0])
+        bias = 1
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+        self.o2 = Neuron(weights, bias)
+
+    def feedforward(self, inputs):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+        out_o2 = self.o2.feedforward(np.array([out_h1, out_h2]))
+        return out_o1, out_o2
+
+
+network = OurNeuralNetwork()
+x = np.array([2, 3])
+print(network.feedforward(x))
+
+# 1.1.2(1.2)
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+
+class OurNeuralNetwork:
+    def __init__(self):
+        weights = np.array([1, 0])
+        bias = 1
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+        self.o2 = Neuron(weights, bias)
+
+    def feedforward(self, inputs):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+        out_o2 = self.o2.feedforward(np.array([out_h1, out_h2]))
+        return out_o1, out_o2
+
+
+network = OurNeuralNetwork()
+x = np.array([2, 3])
+print(network.feedforward(x))
+
 # 1.2
 import matplotlib.pyplot as plt
 import pandas as pd
